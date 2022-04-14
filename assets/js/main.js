9cb33bb39855df1a1e533dc4d282c7a321191bae
EliasFredoni <fredoni.elias@gmail.com>
2022-04-14 15:30:02 +0200

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+document.getElementById("gallery").innerHTML = '<figure><img src="https://unsplash.it/200/200" alt="SelfHTML"><figcaption>Fig.1</figcaption></figure>'
+
+document.getElementById("gallery").innerHTML += '<figure><img src="https://unsplash.it/200/200" alt="SelfHTML"><figcaption>Fig.2</figcaption></figure>'
+
+document.getElementById("gallery").innerHTML += '<figure><img src="https://unsplash.it/200/200" alt="SelfHTML"><figcaption>Fig.3</figcaption></figure>'
+
+
+
+
+
+
